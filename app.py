5b24f076c30552ebfaf48d3d59a10d29efae826f
TilaK Bonala <tilakbonala99@gmail.com>
2025-12-27 23:50:22 -0500

    v10

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ async def training():
 async def predict_route(request: TextInput):
     obj = PredictionPipeline()
     summary = obj.predict(request.text)
-    return {"summary": summary}
+    return summary
 
 if __name__=="__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
